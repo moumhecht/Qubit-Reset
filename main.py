7785076886d89c4e58d20f1de706db2412faa6e7
moumhecht <malida.hecht@gmail.com>
2026-02-12 11:33:40 -0800

    initial commit includes draft readme and main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Main Initialization File and Configuration File
+
+'''
+
+#%% Importing Libraries
+from scipy.signal.windows import gaussian
+from waveform_tools import *
+from qm.qua import *
+from qm import LoopbackInterface
+from qm import QuantumMachinesManager
+from qm import SimulationConfig
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Qubit and Cavity Reset Measurement Experiment Code
+
+Repo for measuring qubit and cavity reset using OPX1.
